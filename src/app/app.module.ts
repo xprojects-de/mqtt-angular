fa6e0390822912c53526062e23d2afc4999e1ee9
XProjects <infos@x-projects.de>
2020-03-22 21:20:52 +0100

    trying TLS but not working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
 
-import { MqttModule, IMqttServiceOptions } from "ngx-mqtt";
+//const KEY = './Users/xprojects/Documents/Github/mqtt-java/MQTT-Java/hivemq/certs/mqtt-client-key.pem';
+//const CERT = './Users/xprojects/Documents/Github/mqtt-java/MQTT-Java/hivemq/certs/mqtt-client-cert.pem';
+//const CA = './Users/xprojects/Documents/Github/mqtt-java/MQTT-Java/hivemq/certs/server.pem';
+
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
   hostname: 'localhost',
   port: 8000,
+  protocol: 'ws',
   path: '/mqtt'
 }
+/*export const MQTT_SERVICE_OPTIONS_TLS: IMqttServiceOptions = {
+  hostname: 'localhost',
+  port: 8001,
+  path: '/mqtt',
+  protocol: 'wss',
+  ca: CA,
+  cert: CERT,
+  key: KEY,
+  rejectUnauthorized: false
+}*/
 
 @NgModule({
   declarations: [
